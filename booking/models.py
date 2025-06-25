864a1ba84e7695f257850380e148054119b7a72f
Zhen Keat <zchua2@huskers.unl.edu>
2025-06-25 05:37:49 -0500

    some basic setup, created booking app and setup its model

---
+++ booking/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Client(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+
+class Appointment(models.Model):
+    start_time = models.DateTimeField()
+    status = models.CharField(max_length=20, default="confirmed")
